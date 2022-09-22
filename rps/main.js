b7889707457c04fd8ea7f3d88660171bd90b18a5
JP <jfinnegan@hvilletn.org>
2022-09-22 15:36:09 -0500

    adding working game loop
    was able to correct and finish a working function
    to start the game loop. Allow entry of a corrected
    entry for ROCK, PAPER, SCISSORS, and corrects capitalization
    to fit within the game mechanics. Added functionality to
    continue.

--- rps/main.js
+++ rps/main.js
@@ -52,12 +52,73 @@ console.log(" ---------- Let's Play Rock! Paper! Scissors! ---------- ");
 console.log("The best of 5 rounds wins the game! Good Luck!")
 
 
+console.log("Let's play a round!");
 
-let playerSelection = console.log(prompt("Enter you selection here: "));
 
 
+
+
+//this is where the game loop should go
+
+// function game() {}
+
+//Game loop works~!
+
+function game() {
+    let playerSelection = prompt("Enter you selection here: ");
+
+    if (playerSelection.toUpperCase() === "ROCK" || playerSelection.toUpperCase() === "PAPER" || playerSelection.toUpperCase() === "SCISSORS") {
+            console.log("You have entered the game");
+        } else if (playerSelection.toUpperCase() !== "ROCK" || playerSelection.toUpperCase() !== "PAPER" || playerSelection.toUpperCase() !== "SCISSORS") {
+            game();
+        } else {
+            console.log("You have not entered the game")
+    }
+}
+
+
+game();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+let computerSelection;
+
+let playerGuess = console.log(toString(playerSelection)); // console.log(playerGuess.toUpperCase());
+
+
+const rpsSelect  = ['ROCK', 'PAPER', 'SCISSORS'] ;
 
 //aparently to get playRound() to work, you need to allow for in the input to always be set to a string, even if empty, henc || " "
+
 
 if (playerSelection != null && playerSelection != undefined) {
     playerSelection = playerSelection;
@@ -65,16 +126,6 @@ if (playerSelection != null && playerSelection != undefined) {
     playerSelection = '';
 }
 
-
-
-let playerGuess = "oh boy donuts";
-
-console.log(playerGuess.toUpperCase());
-
-/*
-let computerSelection;
-
-const rpsSelect  = ['ROCK', 'PAPER', 'SCISSORS'] ;
 
 // shoot function utilizes the Math.random feature by selecting a random integer within the array index!
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -91,22 +142,33 @@ function shoot(min, max) {
 }
 
 
+function getComputerChoice(computerSelection) {
+    console.log(shoot());
+}
+
+
 function compare() { 
     console.log('Booya');
 }
 
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerGuess, computerSelection) {
 
-    if (playerGuess == rpsSelect[0] || playerGuess === rpsSelect[1] || playerGuess === rpsSelect[2]) {
+    if (playerGuess) {
+        
+    }
+
+    computerSelection = console.log(shoot());
+
+    if (playerGuess === 'ROCK' || playerGuess === 'PAPER' || playerGuess === 'SCISSORS') {
         console.log(compare()); 
     } else {
         console.log(prompt("Please Enter either Rock, Paper, or Scissors: "));
     }
 
-    // console.log("Let's play a round!");
+    
 
-    // computerSelection = console.log(shoot());
+    
 
    
 
